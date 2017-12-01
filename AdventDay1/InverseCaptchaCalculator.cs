1a1c37d976c6406b4a036da40f1eeb0c90136889
Sean Manton <sean.manton@thelevelup.com>
2017-12-01 12:32:57 -0500

    Add project files.

---
+++ AdventDay1/InverseCaptchaCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDay1
+{
+    public class InverseCaptchaCalculator
+    {
+        private int _sum = 0;
+
+        public int SumRepeatedDigits(string captcha)
+        {
+            for (int i = 0; i < captcha.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
