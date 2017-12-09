0b8f792b8b8f40d9eea9d45e472f70c394a76101
Sean Manton <sean@seanxmanton.com>
2017-12-09 16:24:58 -0500

    Day 7 kinda messy but works

---
+++ AdventDay7/ProgramListParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AdventDay7
+{
+    public static class ProgramListParser
+    {
+        private const string REGEX = @"(?<name>\S+)\s+\((?<weight>\d+)\)(?:\s->\s)*(?<children>.*)";
+        private static Regex _regex = new Regex(REGEX);
+
+        public static List<ProgramModel> GetProgramModelsFromFile(string file)
+        {
+            var programList = new List<ProgramModel>();
+            using (StreamReader reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var programModel = ParseLine(reader.ReadLine());
+                    programList.Add(programModel);
+                }
+            }
+
+            foreach (ProgramModel program in programList)
+            {
+                foreach (string name in program.ChildrenNames)
+                {
+                    var childProgram = programList.First(p => p.Name == name);
+                    program.Children.Add(childProgram);
+                    childProgram.Parent = program;
+                }
+            }
+
+            return programList;
+        }
+
+        public static ProgramModel ParseLine(string line)
+        {
+            var match = _regex.Match(line);
+            if (match.Success)
+            {
+                var programModel = new ProgramModel
+                {
+                    Name = match.Groups["name"].Value,
+                    Weight = int.Parse(match.Groups["weight"].Value)
+                };
+                var childrenNames = match.Groups["children"].Value;
+                if (!string.IsNullOrWhiteSpace(childrenNames))
+                {
+                    programModel.ChildrenNames.AddRange(childrenNames.Split(',').Select(s => s.Trim()));
+                }
+                return programModel;
+            }
+            throw new ArgumentException("Could not parse the line as a ProgramModel");
+        }
+    }
+}
---
+++ AdventDay7/ProgramModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDay7
+{
+    public class ProgramModel
+    {
+        public string Name { get; set; }
+
+        public ProgramModel Parent { get; set; }
+
+        public List<ProgramModel> Children { get; } = new List<ProgramModel>();
+
+        public List<string> ChildrenNames { get; } = new List<string>();
+
+        public int Weight { get; set; }
+
+        public int WeightIncludingAllChildren => Weight + Children.Sum(c => c.WeightIncludingAllChildren);
+    }
+}
