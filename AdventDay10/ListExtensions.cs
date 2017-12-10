2a7ae61b50cfdd71a849d87445162a53e8beecda
Sean Manton <sean@seanxmanton.com>
2017-12-10 13:52:14 -0500

    Day 10 complete

---
+++ AdventDay10/ListExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDay10
+{
+    public static class ListExtensions
+    {
+        public static List<T> GetRangeCircular<T>(this List<T> list, int start, int count)
+        {
+            var subset = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                int index = (start + i) % list.Count;
+                subset.Add(list[index]);
+            }
+            return subset;
+        }
+
+        public static void ReplaceRangeCircular<T>(this List<T> list, List<T> replacement, int start)
+        {
+            for (int i = 0; i < replacement.Count; i++)
+            {
+                int index = (start + i) % list.Count;
+                list[index] = replacement[i];
+            }
+        }
+    }
+}
---
+++ AdventDay10/ListGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDay10
+{
+    public static class ListGenerator
+    {
+        public static List<int> MakeValueEqualsIndexList(int count)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i);
+            }
+
+            return list;
+        }
+    }
+}
